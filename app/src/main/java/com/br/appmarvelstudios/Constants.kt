f7b842de454ec837f47579095a14db037f05d2e5
Samuel Ribeiro <samuelribeiro9@icloud.com>
2022-08-17 00:34:19 -0300

    teste

--- app/src/main/java/com/br/appmarvelstudios/Constants.kt
+++ app/src/main/java/com/br/appmarvelstudios/Constants.kt
@@ -2,7 +2,7 @@ package com.br.appmarvelstudios
 
 class Constants {
     companion object {
-        const val TIMES_TAMP = "1"         
+        const val TIMES_TAMP = "1"
         const val APIKEY = "25bed2446497b4e7cbbfc19969af2c2a"
         const val HASH_MD5 = "45ab393ef69a6c7523c54b337869adab"
         const val NAME_BANK_OF_DICE = "characters-marvel.db"
